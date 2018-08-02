7bd41b526bf3a74c191808d545274c4a64e1dbec
钟浩良 <>
2018-08-02 14:42:44 +0800

    初始化项目

---
+++ HLTabViewController/HLTabViewController.h
@@ -0,0 +1,19 @@
+//
+//  HLTabViewController.h
+//  HLTabViewController
+//
+//  Created by 钟浩良 on 2018/8/2.
+//  Copyright © 2018年 肇庆市华盈体育文化发展有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HLTabViewController.
+FOUNDATION_EXPORT double HLTabViewControllerVersionNumber;
+
+//! Project version string for HLTabViewController.
+FOUNDATION_EXPORT const unsigned char HLTabViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HLTabViewController/PublicHeader.h>
+
+
